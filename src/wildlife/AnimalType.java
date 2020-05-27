4a64efcd02362ed6ada5b1125b41d35abe94699a
oobee85 <oobee85@gmail.com>
2020-05-26 19:56:25 -0700

    fixed images

--- src/wildlife/AnimalType.java
+++ src/wildlife/AnimalType.java
@@ -12,7 +12,7 @@ public enum AnimalType implements HasImage{
 	HORSE("resources/Images/units/horse.png", new CombatStats(100, 5, 10, 5), false, false, false),
 	PIG    ("resources/Images/units/pig.png", new CombatStats(100, 5, 10, 5), false, false, false),
 	SHEEP("resources/Images/units/sheep.png", new CombatStats(100, 5, 10, 5), false, false, false),
-	FISH("resources/Images/units/fish2.png", new CombatStats(10, 1, 100, 1), true, false, false),
+	FISH ("resources/Images/units/fish2.png", new CombatStats(10, 1, 100, 1), true, false, false),
 	
 	DRAGON("resources/Images/units/dragon.png", new CombatStats(1000, 50, 100, 50), false, true, true),
 	WOLF    ("resources/Images/units/wolf.png", new CombatStats(100, 10, 10, 10), false, false, true),
