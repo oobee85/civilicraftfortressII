f577d7e7291ffc1012ec08a8a14af425cd531892
oobee85 <oobee85@gmail.com>
2020-08-27 16:41:17 -0700

    changed mine rate for ores

--- src/world/ResourceType.java
+++ src/world/ResourceType.java
@@ -8,16 +8,16 @@ import utils.*;
 
 public enum ResourceType implements HasImage {
 	
-	COPPER ( 100, new String[] {"resources/Images/resources/copper/ore_copper16.png", "resources/Images/resources/copper/ore_copper128.png"} , 			5, false, ItemType.COPPER_ORE, true, 10),
-	SILVER ( 100, new String[] {"resources/Images/resources/silver/ore_silver16.png", "resources/Images/resources/silver/ore_silver128.png"} ,			 4, false, ItemType.SILVER_ORE, true, 10),
-	IRON ( 100, new String[] {"resources/Images/resources/iron/ore_iron16.png", "resources/Images/resources/iron/ore_iron128.png"}, 						4, false, ItemType.IRON_ORE, true, 10),
+	COPPER ( 100, new String[] {"resources/Images/resources/copper/ore_copper16.png", "resources/Images/resources/copper/ore_copper128.png"} , 			5, false, ItemType.COPPER_ORE, true, 20),
+	SILVER ( 100, new String[] {"resources/Images/resources/silver/ore_silver16.png", "resources/Images/resources/silver/ore_silver128.png"} ,			 4, false, ItemType.SILVER_ORE, true, 20),
+	IRON ( 100, new String[] {"resources/Images/resources/iron/ore_iron16.png", "resources/Images/resources/iron/ore_iron128.png"}, 						4, false, ItemType.IRON_ORE, true, 20),
 	
-	MITHRIL ( 100, new String[] {"resources/Images/resources/mithril/ore_mithril16.png", "resources/Images/resources/mithril/ore_mithril128.png"} , 		2, false, ItemType.MITHRIL_ORE, true, 10),
-	GOLD ( 100, new String[] {"resources/Images/resources/gold/ore_gold16.png", "resources/Images/resources/gold/ore_gold128.png"} , 						2, true, ItemType.GOLD_ORE, true, 10),
-	ADAMANTITE ( 100, new String[] {"resources/Images/resources/adamantite/ore_adamant16.png", "resources/Images/resources/adamantite/ore_adamant128.png"} , 1, true, ItemType.ADAMANTITE_ORE, true, 10),
+	MITHRIL ( 100, new String[] {"resources/Images/resources/mithril/ore_mithril16.png", "resources/Images/resources/mithril/ore_mithril128.png"} , 		2, false, ItemType.MITHRIL_ORE, true, 40),
+	GOLD ( 100, new String[] {"resources/Images/resources/gold/ore_gold16.png", "resources/Images/resources/gold/ore_gold128.png"} , 						2, true, ItemType.GOLD_ORE, true, 40),
+	ADAMANTITE ( 100, new String[] {"resources/Images/resources/adamantite/ore_adamant16.png", "resources/Images/resources/adamantite/ore_adamant128.png"} , 1, true, ItemType.ADAMANTITE_ORE, true, 40),
 	
-	RUNITE ( 100, new String[] {"resources/Images/resources/runite/ore_rune16.png", "resources/Images/resources/runite/ore_rune128.png"} , 					1, true, ItemType.RUNITE_ORE, true, 10),
-	TITANIUM ( 100, new String[] {"resources/Images/resources/titanium/ore_titanium16.png", "resources/Images/resources/titanium/ore_titanium128.png"} , 	1, true, ItemType.TITANIUM_ORE, true, 10),
+	RUNITE ( 100, new String[] {"resources/Images/resources/runite/ore_rune16.png", "resources/Images/resources/runite/ore_rune128.png"} , 					1, true, ItemType.RUNITE_ORE, true, 100),
+	TITANIUM ( 100, new String[] {"resources/Images/resources/titanium/ore_titanium16.png", "resources/Images/resources/titanium/ore_titanium128.png"} , 	1, true, ItemType.TITANIUM_ORE, true, 100),
 	
 	DEAD_ANIMAL(100, new String[] {"resources/Images/resources/dead_animal.png"} 																		, 0, true, ItemType.FOOD, false, 10),
 	;
