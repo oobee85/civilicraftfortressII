9b9df393ddffb43d0e38c0fed50bec608d3a6984
oobee85 <oobee85@gmail.com>
2020-09-07 23:23:23 -0700

    changed researches

--- src/game/ResearchType.java
+++ src/game/ResearchType.java
@@ -11,22 +11,20 @@ public enum ResearchType implements HasImage {
 	BRONZE_WORKING (100, "resources/Images/units/spearman.png", new ResearchType[] {}),
 	WRITING (100, "resources/Images/research/writing.png", new ResearchType[] {}),
 	WHEEL (100, "resources/Images/research/wheel.png", new ResearchType[] {}),
-	WOODCUTTING (100, "resources/Images/buildings/sawmill.png", new ResearchType[] {}),
 	
 	IRON_WORKING (200, "resources/Images/research/iron_working.png", new ResearchType[] {ResearchType.WARRIOR_CODE, ResearchType.BRONZE_WORKING}),
 	FARMING (200, "resources/Images/buildings/farm.png", new ResearchType[] {ResearchType.BRONZE_WORKING}),
-	MASONRY (200, "resources/Images/buildings/wall_stone.png", new ResearchType[] {ResearchType.WHEEL, ResearchType.WOODCUTTING}),
-	MYSTICISM (200, "resources/Images/research/mysticism.png",  new ResearchType[] {ResearchType.WOODCUTTING}),
 	MATHEMATICS (200, "resources/Images/research/mathematics.png", new ResearchType[] {ResearchType.WRITING, ResearchType.WHEEL}),
+	MASONRY (200, "resources/Images/buildings/stonewall.png", new ResearchType[] {ResearchType.WHEEL}),
 	
 	ARMORING (300, "resources/Images/research/armoring.png", new ResearchType[] {ResearchType.IRON_WORKING}),
-	HORSEBACK_RIDING (300, "resources/Images/research/horseback_riding.png", new ResearchType[] {ResearchType.FARMING}),
+	HORSEBACK_RIDING (300, "resources/Images/research/horseback_riding.png", new ResearchType[] {ResearchType.FARMING, ResearchType.IRON_WORKING}),
 	CURRENCY (300, "resources/Images/itemicons/gold.png", new ResearchType[] {ResearchType.FARMING, ResearchType.MATHEMATICS}),
-	CONSTRUCTION (300, "resources/Images/buildings/wall_brick.png", new ResearchType[] {ResearchType.MASONRY, ResearchType.MATHEMATICS}),
+	ENGINEERING (300, "resources/Images/buildings/wall_brick.png", new ResearchType[] {ResearchType.MASONRY, ResearchType.MATHEMATICS}),
+	MYSTICISM (300, "resources/Images/research/mysticism.png",  new ResearchType[] {ResearchType.MASONRY}),
 	
 	CHIVALRY (400, "resources/Images/units/knight.png", new ResearchType[] {ResearchType.ARMORING, ResearchType.HORSEBACK_RIDING}),
-	CODE_OF_LAWS (400, "resources/Images/research/code_of_laws.png", new ResearchType[] {ResearchType.CURRENCY}),
-	MONARCHY (400, "resources/Images/research/monarchy.png", new ResearchType[] {ResearchType.CONSTRUCTION, ResearchType.MYSTICISM}),
+	MONARCHY (400, "resources/Images/research/monarchy.png", new ResearchType[] {ResearchType.ENGINEERING, ResearchType.CURRENCY, ResearchType.MYSTICISM}),
 	;
 	
 	private ResearchType[] children;
