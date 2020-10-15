4eec02b2cac29393e88f63904dceab982f37234b
oobee85 <oobee85@gmail.com>
2020-10-14 17:23:11 -0700

    working on trader

--- src/game/UnitType.java
+++ src/game/UnitType.java
@@ -27,31 +27,34 @@ public enum UnitType implements HasImage {
  	CHARIOT         ( "resources/Images/units/chariot.png", new CombatStats(300, 30, 10, 5, 30, 300, 50), false, false, false, false, false, "HORSEBACK_RIDING", new HashMap<ItemType, Integer>() { {put(ItemType.FOOD,100); put(ItemType.WOOD,200); put(ItemType.HORSE,20); put(ItemType.IRON_BAR,10);}}, null, ProjectileType.ARROW_ARCHER ),
  	HORSEARCHER ( "resources/Images/units/horsearcher.png", new CombatStats(300, 0, 5, 3, 20, 500, 50),  false, false, false, false, false, "CHIVALRY", new HashMap<ItemType, Integer>() { {put(ItemType.FOOD,100); put(ItemType.HORSE,50); put(ItemType.RUNITE_BAR,5);}}, null, ProjectileType.RUNE_ARROW ),
  	
-	DEER		("resources/Images/units/deer.png", 	new CombatStats(200, 5, 5, 1, 40, 0, 50), 	false, false, false, false, false, null, null, new Item(100, ItemType.FOOD), null),
-	HORSE		("resources/Images/units/horse.png",	new CombatStats(200, 5, 5, 1, 40, 0, 50), 	false, false, false, false, false, null, null, new Item(100, ItemType.FOOD), null),
+	DEER		("resources/Images/units/deer.png", 	new CombatStats(200, 5, 5, 1, 40, 0, 50), 	false, false, false, false, false, null, null, new Item(200, ItemType.FOOD), null),
+	HORSE		("resources/Images/units/horse.png",	new CombatStats(200, 5, 5, 1, 40, 0, 50), 	false, false, false, false, false, null, null, new Item(200, ItemType.FOOD), null),
 	PIG			("resources/Images/units/pig.png",		new CombatStats(200, 10, 30, 1, 40, 0, 50), false, false, false, false, false, null, null, new Item(200, ItemType.FOOD), null),
-	SHEEP		("resources/Images/units/sheep.png",	new CombatStats(200, 5, 30, 1, 40, 0, 50), 	false, false, false, true, false, null, null, new Item(100, ItemType.FOOD), null),
+	SHEEP		("resources/Images/units/sheep.png",	new CombatStats(200, 5, 30, 1, 40, 0, 50), 	false, false, false, true, false, null, null, new Item(200, ItemType.FOOD), null),
 	COW			("resources/Images/units/cow.png",		new CombatStats(200, 10, 30, 1, 40, 0, 50), false, false, false, false, false, null, null, new Item(200, ItemType.FOOD), null),
-	FISH		("resources/Images/units/fish2.png",	new CombatStats( 10, 1, 5, 1, 40, 0, 50),  	true, false, false, true, false, null, null, new Item(100, ItemType.FOOD), null),
+	FISH		("resources/Images/units/fish2.png",	new CombatStats( 10, 1, 5, 1, 40, 0, 50),  	true, false, false, true, false, null, null, new Item(200, ItemType.FOOD), null),
 	
 	DRAGON	   ("resources/Images/units/dragon.png",	new CombatStats(2000, 50, 5, 1, 30, 0, 10), 	false, true, true, false, true, null, null, new Item(10, ItemType.RUNITE_ORE), ProjectileType.FIREBALL_DRAGON),
-	VAMPIRE    ("resources/Images/units/vampire.png", 	new CombatStats(200, 20, 15, 1, 40, 0, 1), 		false, true, true, false, false, null, null, new Item(100, ItemType.FOOD), null),
-	ROC			("resources/Images/units/roc.png",		new CombatStats(500, 50, 10, 1, 25, 0, 10), 	false, true, true, true, false, null, null, new Item(100, ItemType.FOOD), null),
+	VAMPIRE    ("resources/Images/units/vampire.png", 	new CombatStats(200, 20, 15, 1, 40, 0, 1), 		false, true, true, false, false, null, null, new Item(1, ItemType.IRON_BAR), null),
+	ROC			("resources/Images/units/roc.png",		new CombatStats(500, 50, 10, 1, 25, 0, 10), 	false, true, true, true, false, null, null, new Item(5, ItemType.ADAMANTITE_ORE), null),
 	
-	OGRE	   ("resources/Images/units/ogre.png",		new CombatStats(500, 40, 50, 1, 40, 0, 5), 		false, false, true, false, false, null, null, new Item(10, ItemType.BRONZE_BAR), null),
-	ENT		   ("resources/Images/units/ent.png",		new CombatStats(1000, 50, 50, 1, 100, 0, 10), 	false, false, true, false, false, null, null, new Item(200, ItemType.WOOD), null),
-	WEREWOLF   ("resources/Images/units/werewolf.png",	new CombatStats(100, 25, 10, 1, 5, 0, 10), 		false, false, true, false, false, null, null, null, null),
-	LAVAGOLEM  ("resources/Images/units/lavagolem.png", new CombatStats(1000, 200, 50, 1, 100, 0, 10), 	false, false, true, false, true, null, null, new Item(50, ItemType.MITHRIL_ORE), null),
-	WOLF		("resources/Images/units/wolf.png",		new CombatStats(200, 40, 20, 1, 20, 0, 50), 	false, false, true, true, false, null, null, new Item(100, ItemType.FOOD), null),
-	CYCLOPS    ("resources/Images/units/cyclops.png", 	new CombatStats(500, 40, 50, 6, 40, 0, 10), 	false, false, true, false, false, null, null, new Item(1, ItemType.MITHRIL_BAR), ProjectileType.ROCK_CYCLOPS),
-	SKELETON	("resources/Images/units/skeleton.png",	new CombatStats(200, 20, 20, 1, 20, 0, 50), 	false, false, true, false, false, null, null, new Item(100, ItemType.FOOD), null),
-	ICE_GIANT	("resources/Images/units/icegiant.png",	new CombatStats(500, 50, 50, 1, 20, 0, 10), 	false, false, true, true, false, null, null, new Item(1, ItemType.MITHRIL_BAR), null),
+	OGRE	   ("resources/Images/units/ogre.png",		new CombatStats(500, 40, 50, 1, 40, 0, 5), 		false, false, true, false, false, null, null, new Item(10, ItemType.IRON_BAR), null),
+	ENT		   ("resources/Images/units/ent.png",		new CombatStats(1000, 25, 50, 1, 50, 0, 10), 	false, false, true, false, false, null, null, new Item(500, ItemType.WOOD), null),
+	WEREWOLF   ("resources/Images/units/werewolf.png",	new CombatStats(100, 25, 10, 1, 5, 0, 10), 		false, false, true, false, false, null, null, new Item(1, ItemType.IRON_BAR), null),
+	LAVAGOLEM  ("resources/Images/units/lavagolem.png", new CombatStats(1000, 150, 50, 1, 50, 0, 10), 	false, false, true, false, true, null, null, new Item(50, ItemType.MITHRIL_ORE), null),
+	WOLF		("resources/Images/units/wolf.png",		new CombatStats(200, 40, 20, 1, 20, 0, 50), 	false, false, true, true, false, null, null, new Item(300, ItemType.FOOD), null),
+	CYCLOPS    ("resources/Images/units/cyclops.png", 	new CombatStats(500, 40, 50, 6, 40, 0, 10), 	false, false, true, false, false, null, null, new Item(10, ItemType.MITHRIL_ORE), ProjectileType.ROCK_CYCLOPS),
+	SKELETON	("resources/Images/units/skeleton.png",	new CombatStats(200, 20, 20, 1, 20, 0, 50), 	false, false, true, false, false, null, null, new Item(1, ItemType.BRONZE_BAR), null),
+	ICE_GIANT	("resources/Images/units/icegiant.png",	new CombatStats(800, 60, 50, 1, 20, 0, 10), 	false, false, true, true, false, null, null, new Item(2, ItemType.MITHRIL_SWORD), null),
 	
-	FLAMELET	    ("resources/Images/units/flamelet.png", new CombatStats(1, 0, 25, 0, 1000, 0, 50), 	false, false, true, false, true, null, null, new Item(10 ,ItemType.COAL), null),
+	FLAMELET	    ("resources/Images/units/flamelet.png", new CombatStats(1, 0, 25, 0, 1000, 0, 50), 	false, false, true, false, true, null, null, new Item(50 ,ItemType.COAL), null),
 	WATER_SPIRIT ("resources/Images/units/waterspirit.png", new CombatStats(1, 0, 25, 0, 1000, 0, 50), 	false, false, false, false, false, null, null, null, null),
 	PARASITE	 ("resources/Images/units/parasite.png", 	new CombatStats(1, 1, 5, 0, 1000, 0, 50), 	false, false, true, false, true, null, null, null, null),
 	FIREFLY			("resources/Images/units/firefly.png",	new CombatStats(1, 0, 25, 1, 1000, 0, 50),  false, false, false, false, false, null, null, null, null),
 	BOMB			("resources/Images/units/bomb.png",		new CombatStats(1, 0, 25, 1, 1000, 0, 50),  false, false, false, false, false, null, null, null, null),
+	
+	TRADER     ( "resources/Images/units/trader.png",		new CombatStats(500, 40, 15, 1, 20, 0, 50), false, false, false, false, false, null, null, new Item(50 ,ItemType.GOLD_ORE), null ),
+ 	
 	;
 	
 	private MipMap mipmap;
