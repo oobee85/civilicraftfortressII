041353bbbc5f5c1bf9cc31d5f8b2f0a091ebc8a3
Oleks Korshak <alexokorshak@gmail.com>
2020-10-21 01:34:43 -0700

    show unsatisfied cost in red

--- src/ui/infopanels/UnitTypeInfoPanel.java
+++ src/ui/infopanels/UnitTypeInfoPanel.java
@@ -58,7 +58,9 @@ public class UnitTypeInfoPanel extends InfoPanel {
 		
 		for(Entry<ItemType, Integer> entry : costs.entrySet()) {
 			g.drawImage(entry.getKey().getImage(iconSize), x, y, iconSize, iconSize, null);
-			String str = entry.getValue() + " " + entry.getKey().toString();
+			int currentAmount = World.PLAYER_FACTION.getItemAmount(entry.getKey());
+			String str = currentAmount + "/" + entry.getValue() + " " + entry.getKey().toString();
+			g.setColor(currentAmount >= entry.getValue() ? Color.black : Color.red);
 			g.drawString(str, x + iconSize + gap + 1, y + iconSize/2 + fontSize/3);
 			y += iconSize + gap;
 		}
