8af60fdfb3c52c918209dfed8fb66f09eebd3b2a
Alex Korshak <alexokorshak@gmail.com>
2021-11-28 14:54:01 -0800

    clean up DebugView code

--- src/ui/view/DebugView.java
+++ src/ui/view/DebugView.java
@@ -57,114 +57,71 @@ public class DebugView {
 		mapModeButtons.getFirst().setSelected(true);
 
 		JToggleButton flipTable = KUIConstants.setupToggleButton("Flip Table", null, DEBUG_BUTTON_SIZE);
-		flipTable.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().flipTable();
-				flipTable.setText(flipTable.isSelected() ? "Unflip Table" : "Flip Table");
-			}
+		flipTable.addActionListener(e -> {
+			gameView.getGameInstance().flipTable();
+			flipTable.setText(flipTable.isSelected() ? "Unflip Table" : "Flip Table");
 		});
 
 		JButton eruptVolcano = KUIConstants.setupButton("Erupt Volcano", ERUPTION_ICON, DEBUG_BUTTON_SIZE);
-		eruptVolcano.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().eruptVolcano();
-			}
+		eruptVolcano.addActionListener(e -> {
+			gameView.getGameInstance().eruptVolcano();
 		});
 		
 		JButton addResources = KUIConstants.setupButton("Give Resources", null, DEBUG_BUTTON_SIZE);
-		addResources.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().addResources(gameView.getFaction());
-			}
+		addResources.addActionListener(e -> {
+			gameView.getGameInstance().addResources(gameView.getFaction());
 		});
 
 		JButton makeItRain = KUIConstants.setupButton("Rain", RAIN_ICON, DEBUG_BUTTON_SIZE);
-		makeItRain.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().world.rain();
-				gameView.getGameInstance().world.grow();
-			
-			}
+		makeItRain.addActionListener(e -> {
+			gameView.getGameInstance().world.rain();
+			gameView.getGameInstance().world.grow();
 		});
 		
 
 		JButton makeItDry = KUIConstants.setupButton("Drought", null, DEBUG_BUTTON_SIZE);
-		makeItDry.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().world.drought();
-			}
+		makeItDry.addActionListener(e -> {
+			gameView.getGameInstance().world.drought();
 		});
 
 		JToggleButton fastForward = KUIConstants.setupToggleButton("Fast Forward", FAST_FORWARD_ICON, DEBUG_BUTTON_SIZE);
-		fastForward.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().getGUIController().setFastForwarding(fastForward.isSelected());
-				fastForward.setText(fastForward.isSelected() ? "Stop Fast Forward" : "Fast Forward");
-			}
+		fastForward.addActionListener(e -> {
+			gameView.getGameInstance().getGUIController().setFastForwarding(fastForward.isSelected());
+			fastForward.setText(fastForward.isSelected() ? "Stop Fast Forward" : "Fast Forward");
 		});
 		JButton raiseHeight = KUIConstants.setupButton("SpawnWeather", RAIN_ICON, DEBUG_BUTTON_SIZE);
-		raiseHeight.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.setWeather(true);
-			}
+		raiseHeight.addActionListener(e -> {
+			gameView.setWeather(true);
 		});
 		JButton increasePressure = KUIConstants.setupButton("pressure", RAIN_ICON, DEBUG_BUTTON_SIZE);
-		increasePressure.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.increasePressure(true);
-			}
+		increasePressure.addActionListener(e -> {
+			gameView.increasePressure(true);
 		});
 		JButton setTerritoryButton = KUIConstants.setupButton("Set Territory", null, DEBUG_BUTTON_SIZE);
-		setTerritoryButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.setSetTerritory(true);
-			}
+		setTerritoryButton.addActionListener(e -> {
+			gameView.setSetTerritory(true);
 		});
 
 		JButton researchEverything = KUIConstants.setupButton("Research All", RESEARCH_TAB_ICON, DEBUG_BUTTON_SIZE);
-		researchEverything.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().researchEverything(gameView.getFaction());
-			}
+		researchEverything.addActionListener(e -> {
+			gameView.getGameInstance().researchEverything(gameView.getFaction());
 		});
 		JButton shadowWordDeath = KUIConstants.setupButton("Shadow Word: Death", SHADOW_WORD_DEATH, DEBUG_BUTTON_SIZE);
-		shadowWordDeath.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().shadowWordDeath(100);
-			}
+		shadowWordDeath.addActionListener(e -> {
+			gameView.getGameInstance().shadowWordDeath(100);
 		});
 		JButton shadowWordPain = KUIConstants.setupButton("Shadow Word: Pain", SHADOW_WORD_PAIN, DEBUG_BUTTON_SIZE);
-		shadowWordPain.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().shadowWordPain(1);
-			}
+		shadowWordPain.addActionListener(e -> {
+			gameView.getGameInstance().shadowWordPain(1);
 		});
 
 		JButton meteor = KUIConstants.setupButton("Meteor", METEOR_ICON, DEBUG_BUTTON_SIZE);
-		meteor.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().meteorStrike();
-			}
+		meteor.addActionListener(e -> {
+			gameView.getGameInstance().meteorStrike();
 		});
 		JButton unitEvents = KUIConstants.setupButton("Unit Events", null, DEBUG_BUTTON_SIZE);
-		unitEvents.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.getGameInstance().shadowWordDeath(1);
-			}
+		unitEvents.addActionListener(e -> {
+			gameView.getGameInstance().shadowWordDeath(1);
 		});
 		JButton setPlayerFaction = KUIConstants.setupButton("Change Faction", CHANGE_FACTION_ICON, DEBUG_BUTTON_SIZE);
 		setPlayerFaction.addActionListener(e -> {
@@ -179,32 +136,25 @@ public class DebugView {
 		});
 
 		JToggleButton debug = KUIConstants.setupToggleButton(gameView.getDrawDebugStrings() ? "Leave Matrix" : "Matrix", null, DEBUG_BUTTON_SIZE);
-		debug.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gameView.setDrawDebugStrings(debug.isSelected());
-				debug.setText(gameView.getDrawDebugStrings() ? "Leave Matrix" : "Matrix");
-			}
+		debug.addActionListener(e -> {
+			gameView.setDrawDebugStrings(debug.isSelected());
+			debug.setText(gameView.getDrawDebugStrings() ? "Leave Matrix" : "Matrix");
 		});
-		JToggleButton toggleNight = KUIConstants.setupToggleButton(Game.DISABLE_NIGHT ? "Night Disabled" : "Night Enabled", NIGHT_ENABLED_ICON,
+		JToggleButton toggleNight = KUIConstants.setupToggleButton(
+				Game.DISABLE_NIGHT ? "Night Disabled" : "Night Enabled", 
+				NIGHT_ENABLED_ICON,
 				DEBUG_BUTTON_SIZE);
 		toggleNight.setSelected(true);
-		toggleNight.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Game.DISABLE_NIGHT = !toggleNight.isSelected();
-				toggleNight.setText(Game.DISABLE_NIGHT ? "Night Disabled" : "Night Enabled");
-				toggleNight.setIcon(Game.DISABLE_NIGHT ? NIGHT_DISABLED_ICON : NIGHT_ENABLED_ICON);
-			}
+		toggleNight.addActionListener(e -> {
+			Game.DISABLE_NIGHT = !toggleNight.isSelected();
+			toggleNight.setText(Game.DISABLE_NIGHT ? "Night Disabled" : "Night Enabled");
+			toggleNight.setIcon(Game.DISABLE_NIGHT ? NIGHT_DISABLED_ICON : NIGHT_ENABLED_ICON);
 		});
 
 		JToggleButton toggleGL = KUIConstants.setupToggleButton(gameView.is3d() ? "3D" : "2D", null, DEBUG_BUTTON_SIZE);
-		toggleGL.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				gameView.switch3d(!gameView.is3d());
-				toggleGL.setText(gameView.is3d() ? "3D" : "2D");
-			}
+		toggleGL.addActionListener(e -> {
+			gameView.switch3d(!gameView.is3d());
+			toggleGL.setText(gameView.is3d() ? "3D" : "2D");
 		});
 
 		JButton enableFirstPerson = KUIConstants.setupButton("FP Mode", null, DEBUG_BUTTON_SIZE);
@@ -213,75 +163,31 @@ public class DebugView {
 		});
 
 		JButton exit = KUIConstants.setupButton("Exit", null, DEBUG_BUTTON_SIZE);
-		exit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				System.exit(0);
-			}
+		exit.addActionListener(e -> {
+			System.exit(0);
 		});
 		JButton save = KUIConstants.setupButton("Save", null, DEBUG_BUTTON_SIZE);
-		save.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				gameView.getGameInstance().saveToFile();
-			}
+		save.addActionListener(e -> {
+			gameView.getGameInstance().saveToFile();
 		});
 		
 		JButton reseedButton = KUIConstants.setupButton("Reseed", null, DEBUG_BUTTON_SIZE);
-		reseedButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					String newSeed = JOptionPane.showInputDialog("Input seed");
-					if(newSeed == null) {
-						return;
-					}
-					long seed = Long.parseLong(newSeed);
-					gameView.getGameInstance().world.reseedTerrain(seed);
+		reseedButton.addActionListener(e -> {
+			try {
+				String newSeed = JOptionPane.showInputDialog("Input seed");
+				if(newSeed == null) {
+					return;
 				}
-				catch(NumberFormatException ex) {
-					
-				}
+				long seed = Long.parseLong(newSeed);
+				gameView.getGameInstance().world.reseedTerrain(seed);
+			}
+			catch(NumberFormatException ex) {
+				
 			}
 		});
 
-    JButton settingsButton = KUIConstants.setupButton("Settings", null, DEBUG_BUTTON_SIZE);
-    settingsButton.addActionListener(e -> {
-      if(settingsWindow != null) {    
-        int sta = settingsWindow.getExtendedState() & ~JFrame.ICONIFIED & JFrame.NORMAL;
-        settingsWindow.setExtendedState(sta);
-        settingsWindow.setAlwaysOnTop(true);
-        settingsWindow.toFront();
-        settingsWindow.requestFocus();
-        settingsWindow.setAlwaysOnTop(false);
-      }
-      else {
-        settingsWindow = new JFrame("Settings");
-        settingsWindow.setLocationRelativeTo(null);
-        settingsWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        settingsWindow.setSize(500, 500);
-        settingsWindow.addWindowListener(new WindowListener() {
-          @Override
-          public void windowClosing(WindowEvent e) { 
-            settingsWindow = null;
-          }
-          @Override public void windowOpened(WindowEvent e) { }
-          @Override public void windowIconified(WindowEvent e) { }
-          @Override public void windowDeiconified(WindowEvent e) { }
-          @Override public void windowDeactivated(WindowEvent e) { }
-          @Override public void windowClosed(WindowEvent e) { }
-          @Override public void windowActivated(WindowEvent e) { }
-        });
-        SettingsMenu settingsMenu = new SettingsMenu(
-            ee -> {
-              settingsWindow.dispose();
-              settingsWindow = null;
-            });
-        settingsMenu.addControlFor(Settings.class);
-        settingsWindow.add(settingsMenu.getContentPanel());
-        settingsWindow.setVisible(true);
-      }
-    });
+		JButton settingsButton = KUIConstants.setupButton("Settings", null, DEBUG_BUTTON_SIZE);
+		settingsButton.addActionListener(e -> setupSettingsMenu() );
 
 		for(JRadioButton button : mapModeButtons) {
 			scrollingPanel.add(button);
@@ -301,14 +207,50 @@ public class DebugView {
 		scrollingPanel.add(raiseHeight);
 		scrollingPanel.add(increasePressure);
 		scrollingPanel.add(setTerritoryButton);
-//		scrollingPanel.add(save); // doesnt currently work
+		scrollingPanel.add(save); // doesnt currently work
 		scrollingPanel.add(exit);
 		scrollingPanel.add(toggleGL);
 		scrollingPanel.add(enableFirstPerson);
 		scrollingPanel.add(shadowWordDeath);
 		scrollingPanel.add(shadowWordPain);
 		scrollingPanel.add(reseedButton);
-    scrollingPanel.add(settingsButton);
+		scrollingPanel.add(settingsButton);
+	}
+	
+	private void setupSettingsMenu() {
+		if (settingsWindow != null) {
+			int sta = settingsWindow.getExtendedState() & ~JFrame.ICONIFIED & JFrame.NORMAL;
+			settingsWindow.setExtendedState(sta);
+			settingsWindow.setAlwaysOnTop(true);
+			settingsWindow.toFront();
+			settingsWindow.requestFocus();
+			settingsWindow.setAlwaysOnTop(false);
+		} 
+		else {
+			settingsWindow = new JFrame("Settings");
+			settingsWindow.setLocationRelativeTo(null);
+			settingsWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			settingsWindow.setSize(500, 500);
+			settingsWindow.addWindowListener(new WindowListener() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					settingsWindow = null;
+				}
+				@Override public void windowOpened(WindowEvent e) { }
+				@Override public void windowIconified(WindowEvent e) { }
+				@Override public void windowDeiconified(WindowEvent e) { }
+				@Override public void windowDeactivated(WindowEvent e) { }
+				@Override public void windowClosed(WindowEvent e) { } 
+				@Override public void windowActivated(WindowEvent e) { }
+			});
+			SettingsMenu settingsMenu = new SettingsMenu(ee -> {
+				settingsWindow.dispose();
+				settingsWindow = null;
+			});
+			settingsMenu.addControlFor(Settings.class);
+			settingsWindow.add(settingsMenu.getContentPanel());
+			settingsWindow.setVisible(true);
+		}
 	}
 	
 	public JPanel getRootPanel() {
