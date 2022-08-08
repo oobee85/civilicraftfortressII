oobee85/civilicraftfortressII

commit 2efb9bc3113245d5c9f14c50337cf8ed715942e7

finished rendering pipeline cleanup

src/ui/graphics/vanilla/VanillaDrawer.java
@@ -1,11 +1,8 @@
 package ui.graphics.vanilla;
 
 import java.awt.*;
-import java.awt.Component;
 import java.awt.event.*;
 import java.awt.image.*;
-import java.util.*;
-import java.util.List;
 import java.util.concurrent.*;
 
 import javax.swing.*;
@@ -17,12 +14,10 @@ import ui.utils.DrawingUtils;
 import ui.view.GameView.*;
 import utils.*;
 import world.*;
-import world.liquid.*;
 
 public class VanillaDrawer extends Drawer {
 
 	private static final int FAST_MODE_TILE_SIZE = 10;
-	private static final int NUM_DEBUG_DIGITS = 3;
 	
 	private static final Image TARGET_IMAGE = Utils.loadImage("Images/interfaces/ivegotyouinmysights.png");
 	private static final Image SKY_BACKGROUND = Utils.loadImage("Images/lightbluesky.png");
@@ -227,207 +222,19 @@ public class VanillaDrawer extends Drawer {
 			renderState.drawh = frozenTileSize;
 
 			for (RenderingStep step : pipelines[state.mapMode.ordinal()].steps) {
-				if (!step.perTile) {
-					renderState.tile = null;
-					renderState.drawx = -1;
-					renderState.drawy = -1;
-					step.render(renderState);
-				}
-				else {
-					for (int i = renderState.lowerX; i <= renderState.upperX; i++) {
-						for (int j = renderState.lowerY; j <= renderState.upperY; j++) {
-							Tile tile = game.world.get(new TileLoc(i, j));
-							if (tile == null) {
-								continue;
-							}
-							renderState.tile = tile;
-							Point drawAt = getDrawingCoords(tile.getLocation());
-							renderState.drawx = drawAt.x;
-							renderState.drawy = drawAt.y;
-							
-							step.render(renderState);
+				step.render(renderState);
+				for (int i = renderState.lowerX; i <= renderState.upperX; i++) {
+					for (int j = renderState.lowerY; j <= renderState.upperY; j++) {
+						Tile tile = game.world.get(new TileLoc(i, j));
+						if (tile == null) {
+							continue;
 						}
+						Point drawAt = RenderingFunctions.getDrawingCoords(tile.getLocation(), state.tileSize);
+						step.render(renderState, tile, drawAt);
 					}
 				}
 			}
-
-			for (Projectile p : game.world.getData().getProjectiles()) {
-				int extra = (int) (frozenTileSize * p.getExtraSize());
-				double ratio = 0.5 * p.getHeight() / p.getMaxHeight();
-				int shadowOffset = (int) (frozenTileSize * ratio / 2);
-				Point drawAt = getDrawingCoords(p.getTile().getLocation());
-				
-				g.drawImage(p.getType().getMipMap().getShadow(0), drawAt.x + shadowOffset,
-						drawAt.y + shadowOffset, frozenTileSize - shadowOffset * 2,
-						frozenTileSize - shadowOffset * 2, null);
-				g.drawImage(p.getType().getMipMap().getImage(0), drawAt.x - extra / 2,
-						drawAt.y - p.getHeight() - extra / 2, frozenTileSize + extra,
-						frozenTileSize + extra, null);
-			}
-			for (WeatherEvent w : game.world.getWeatherEvents()) {
-				
-				Point drawAt = getDrawingCoords(w.getTile().getLocation());
-				g.drawImage(WeatherEventType.RAIN.getMipMap().getImage(0), drawAt.x ,
-						drawAt.y, frozenTileSize, frozenTileSize, null);
-			}
-
-			int indicatorSize = frozenTileSize / 12;
-			int offset = 4;
-			HashMap<Tile, Integer> visited = new HashMap<>();
-			for (Unit unit : game.world.getUnits()) {
-				int count = visited.getOrDefault(unit.getTile(), 0);
-				visited.put(unit.getTile(), count + 1);
-
-				// draws a square for every player unit on the tile
-				Point drawAt = getDrawingCoords(unit.getTile().getLocation());
-				int xx = drawAt.x + offset;
-				int yy = drawAt.y + (indicatorSize + offset) * count + offset;
-				g.setColor(unit.getFaction().color());
-				g.fillRect(xx, yy, indicatorSize, indicatorSize);
-				g.setColor(Color.BLACK);
-				g.drawRect(xx, yy, indicatorSize, indicatorSize);
-				count++;
-			}
-			
-			// draw brightness of tiles as translucent rectangle
-			if (state.mapMode == MapMode.TERRAIN_BIG) {
-				for (int i = renderState.lowerX; i < renderState.upperX; i++) {
-					for (int j = renderState.lowerY; j < renderState.upperY; j++) {
-						Tile tile = game.world.get(new TileLoc(i, j));
-						if (tile == null)
-							continue;
-						double brightness = World.getDaylight() + tile.getBrightness(state.faction);
-						brightness = Math.max(Math.min(brightness, 1), 0);
-						g.setColor(new Color(0, 0, 0, (int) (255 * (1 - brightness))));
-						Point drawAt = getDrawingCoords(tile.getLocation());
-						g.fillRect(drawAt.x, drawAt.y, frozenTileSize, frozenTileSize);
-					}
-				}
-			}
-			if (state.mapMode == MapMode.FLOW) {
-				for (int i = renderState.lowerX; i < renderState.upperX; i++) {
-					for (int j = renderState.lowerY; j < renderState.upperY; j++) {
-						Tile tile = game.world.get(new TileLoc(i, j));
-						if (tile == null)
-							continue;
-						drawAirFlow(g, tile, true);
-					}
-				}
-			}
-			if (state.mapMode == MapMode.FLOW2) {
-				for (int i = renderState.lowerX; i < renderState.upperX; i++) {
-					for (int j = renderState.lowerY; j < renderState.upperY; j++) {
-						Tile tile = game.world.get(new TileLoc(i, j));
-						if (tile == null)
-							continue;
-						drawAirFlow(g, tile, false);
-					}
-				}
-			}
-
-			if (state.drawDebugStrings) {
-				if (frozenTileSize >= 150) {
-					drawDebugStrings(g, renderState.lowerX, renderState.lowerY, renderState.upperX, renderState.upperY);
-				}
-			}
-			if (state.leftClickAction == LeftClickAction.ATTACK) {
-				drawTarget(g, state.hoveredTile);
-			}
 		}
-	}
-
-	private void drawDebugStrings(Graphics g, int lowerX, int lowerY, int upperX, int upperY) {
-		if(upperX - lowerX <= 0 || upperY - lowerY <= 0) {
-			return;
-		}
-		int[][] rows = new int[upperX - lowerX][upperY - lowerY];
-		int fontsize = frozenTileSize / 4;
-		fontsize = Math.min(fontsize, 13);
-		Font font = new Font("Consolas", Font.PLAIN, fontsize);
-		g.setFont(font);
-		for (int i = lowerX; i < upperX; i++) {
-			for (int j = lowerY; j < upperY; j++) {
-				Tile tile = game.world.get(new TileLoc(i, j));
-				Point drawAt = getDrawingCoords(tile.getLocation());
-				List<String> strings = new LinkedList<String>();
-				strings.add(String.format("H=%." + NUM_DEBUG_DIGITS + "f", tile.getHeight()));
-				strings.add(String.format("PRES" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getPressure()));
-
-//				strings.add(String.format("HUM" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getHumidity()));
-				strings.add(String.format("TTEM" + "=%." + NUM_DEBUG_DIGITS + "f", (tile.getTemperature() - Constants.FREEZETEMP)));
-				strings.add(String.format("ATEM" + "=%." + NUM_DEBUG_DIGITS + "f", (tile.getAir().getTemperature() - Constants.FREEZETEMP)));
-				strings.add(String.format("TENE" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getEnergy()));
-				strings.add(String.format("AENE" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getEnergy()));
-				
-				
-//				strings.add(String.format("EVAP" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getEvaporation()));
-//				strings.add(String.format("dVOL" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getVolumeChange()));
-				strings.add(String.format("VOL" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getVolumeLiquid()));
-				strings.add(String.format("MVOL" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getMaxVolumeLiquid()));
-				
-//				strings.add(String.format("RH" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getRelativeHumidity()));
-//				strings.add(String.format("DEW" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getDewPoint()));
-
-//				strings.add(String.format("MASS" + "=%." + NUM_DEBUG_DIGITS + "f", tile.getAir().getMass()));
-				if (tile.getResource() != null) {
-					strings.add(String.format("ORE" + "=%d", tile.getResource().getYield()));
-				}
-
-				if (tile.liquidType != LiquidType.DRY) {
-					strings.add(String.format(tile.liquidType.name().charAt(0) + "=%." + NUM_DEBUG_DIGITS + "f",
-							tile.liquidAmount));
-				}
-
-				if (tile.getModifier() != null) {
-					strings.add("GM=" + tile.getModifier().timeLeft());
-				}
-				rows[i - lowerX][j - lowerY] = tile.drawDebugStrings(g, strings, rows[i - lowerX][j - lowerY], fontsize,
-						frozenTileSize, drawAt);
-
-				for (Unit unit : tile.getUnits()) {
-					rows[i - lowerX][j - lowerY] = tile.drawDebugStrings(g, unit.getDebugStrings(),
-							rows[i - lowerX][j - lowerY], fontsize, frozenTileSize, drawAt);
-				}
-				if (tile.getPlant() != null) {
-					rows[i - lowerX][j - lowerY] = tile.drawDebugStrings(g, tile.getPlant().getDebugStrings(),
-							rows[i - lowerX][j - lowerY], fontsize, frozenTileSize, drawAt);
-				}
-				if (tile.hasBuilding()) {
-					rows[i - lowerX][j - lowerY] = tile.drawDebugStrings(g, tile.getBuilding().getDebugStrings(),
-							rows[i - lowerX][j - lowerY], fontsize, frozenTileSize, drawAt);
-				}
-				if (tile.getRoad() != null) {
-					rows[i - lowerX][j - lowerY] = tile.drawDebugStrings(g, tile.getRoad().getDebugStrings(),
-							rows[i - lowerX][j - lowerY], fontsize, frozenTileSize, drawAt);
-				}
-			}
-		}
-	}
-
-	private Point getDrawingCoords(TileLoc tileLoc) {
-		int x = tileLoc.x() * frozenTileSize;
-		int y = tileLoc.y() * frozenTileSize + (tileLoc.x() % 2) * frozenTileSize / 2;
-		return new Point(x, y);
-	}
-
-	private void drawTarget(Graphics g, TileLoc tileLoc) {
-		Point drawAt = getDrawingCoords(tileLoc);
-		int w = (int) (frozenTileSize * 8 / 10);
-		int hi = (int) (frozenTileSize * 8 / 10);
-		g.drawImage(TARGET_IMAGE, drawAt.x + frozenTileSize * 1 / 10, drawAt.y + frozenTileSize * 1 / 10, w, hi, null);
-	}
-	private void drawAirFlow(Graphics g, Tile tile, boolean arrows) {
-		TileLoc tileLoc = tile.getLocation();
-		Point drawAt = getDrawingCoords(tileLoc);
-		int w = (int) (frozenTileSize * 8 / 10);
-		int hi = (int) (frozenTileSize * 8 / 10);
-		if(tile.getAir().getFlowDirection() != null) {
-			Image image = arrows ? tile.getAir().getFlowDirection().getArrowImage() : tile.getAir().getFlowDirection().getImage() ;
-//			System.out.println(tile.getAir().getFlowDirection());
-			g.drawImage(image, drawAt.x, drawAt.y, frozenTileSize, frozenTileSize, null);
-//			g.drawImage(TARGET_IMAGE, drawAt.x + frozenTileSize * 1 / 10, drawAt.y + frozenTileSize * 1 / 10, w, hi, null);
-		}
-
 	}
 	
 	/**

commit bdd38fc7e3470106899947ab07c6532629fbfbc5

Fix bug caused by commit 2efb9bc31

src/ui/graphics/vanilla/VanillaDrawer.java
@@ -19,9 +19,7 @@ public class VanillaDrawer extends Drawer {
 
 	private static final int FAST_MODE_TILE_SIZE = 10;
 	
-	private static final Image TARGET_IMAGE = Utils.loadImage("Images/interfaces/ivegotyouinmysights.png");
 	private static final Image SKY_BACKGROUND = Utils.loadImage("Images/lightbluesky.png");
-
 	
 	private JPanel canvas;
 	
@@ -33,12 +31,6 @@ public class VanillaDrawer extends Drawer {
 	private volatile int currentBuffer = 0;
 	private Semaphore nextRequested = new Semaphore(1);
 	private Semaphore numAvailable = new Semaphore(0);
-	
-	// must take snapshot of current tile size and view offset to draw a consistent image
-	// otherwise while player is dragging view it will change view offset 
-	// while it is drawing which causes a bunch of issues.
-	private int frozenTileSize;
-	private Position frozenViewOffset = new Position(0, 0);
 	
 	private RenderingPipeline[] pipelines = new RenderingPipeline[MapMode.values().length];
 	
@@ -63,7 +55,7 @@ public class VanillaDrawer extends Drawer {
 				Utils.setTransparency(g, 1);
 				
 				int buffer = currentBuffer;
-				if(state.tileSize == drawnAtTileSize[buffer]) {
+				if(state.volatileTileSize == drawnAtTileSize[buffer]) {
 					g.drawImage(buffers[buffer], 
 							drawnAtOffset[buffer].getIntX() - state.viewOffset.getIntX(), 
 							drawnAtOffset[buffer].getIntY() - state.viewOffset.getIntY(), 
@@ -98,15 +90,15 @@ public class VanillaDrawer extends Drawer {
 					nextRequested.acquire();
 					long startDrawing = System.currentTimeMillis();
 					int next = (currentBuffer + 1) % buffers.length;
-					frozenViewOffset.x = state.viewOffset.x;
-					frozenViewOffset.y = state.viewOffset.y;
-					frozenTileSize = state.tileSize;
+
+					// Must take snapshot of current tile size and view offset to draw a 
+					// consistent image. Otherwise while player is dragging view it will 
+					// change view offset while it is drawing which causes a bunch of issues.
+					drawnAtOffset[next].x = state.viewOffset.x;
+					drawnAtOffset[next].y = state.viewOffset.y;
+					drawnAtTileSize[next] = state.volatileTileSize;
+					drawStuff(buffers[next], drawnAtOffset[next].x, drawnAtOffset[next].y, drawnAtTileSize[next]);
 					
-					drawStuff(buffers[next]);
-					
-					drawnAtOffset[next].x = frozenViewOffset.x;
-					drawnAtOffset[next].y = frozenViewOffset.y;
-					drawnAtTileSize[next] = frozenTileSize;
 					long finishedDrawing = System.currentTimeMillis();
 					bufferDrawTimes[next] = (int) (finishedDrawing - startDrawing);
 					currentBuffer = next;
@@ -178,7 +170,7 @@ public class VanillaDrawer extends Drawer {
 		}
 	}
 
-	private void drawStuff(BufferedImage image) {
+	private void drawStuff(BufferedImage image, double viewOffsetX, double viewOffsetY, int tileSize) {
 		Graphics2D g = image.createGraphics();
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
@@ -190,36 +182,36 @@ public class VanillaDrawer extends Drawer {
 			g.drawString("No World to display", 20, 20);
 		}
 		else {
-			g.translate(-frozenViewOffset.getIntX(), -frozenViewOffset.getIntY());
-			draw(g, canvas.getWidth(), canvas.getHeight());
-			g.translate(frozenViewOffset.getIntX(), frozenViewOffset.getIntY());
+			g.translate(-viewOffsetX, -viewOffsetY);
+			draw(g, canvas.getWidth(), canvas.getHeight(), tileSize);
+			g.translate(viewOffsetX, viewOffsetY);
 		}
 		g.dispose();
 	}
 
-	private void draw(Graphics g, int panelWidth, int panelHeight) {
+	private void draw(Graphics g, int panelWidth, int panelHeight, int tileSize) {
 		RenderingState renderState = new RenderingState();
 		// Start by drawing plain terrain image
 		g.drawImage(mapImages[state.mapMode.ordinal()], 0, 0, 
-				frozenTileSize * game.world.getWidth(), 
-				frozenTileSize * game.world.getHeight() + frozenTileSize/2, null);
+				tileSize * game.world.getWidth(), 
+				tileSize * game.world.getHeight() + tileSize/2, null);
 
 		// Try to only draw stuff that is visible on the screen
-		renderState.lowerX = Math.max(0, state.viewOffset.divide(frozenTileSize).getIntX() - 2);
-		renderState.lowerY = Math.max(0, state.viewOffset.divide(frozenTileSize).getIntY() - 2);
-		renderState.upperX = Math.min(game.world.getWidth(), renderState.lowerX + panelWidth / frozenTileSize + 4);
-		renderState.upperY = Math.min(game.world.getHeight(), renderState.lowerY + panelHeight / frozenTileSize + 4);
+		renderState.lowerX = Math.max(0, state.viewOffset.divide(tileSize).getIntX() - 2);
+		renderState.lowerY = Math.max(0, state.viewOffset.divide(tileSize).getIntY() - 2);
+		renderState.upperX = Math.min(game.world.getWidth(), renderState.lowerX + panelWidth / tileSize + 4);
+		renderState.upperY = Math.min(game.world.getHeight(), renderState.lowerY + panelHeight / tileSize + 4);
 
-		if (frozenTileSize >= FAST_MODE_TILE_SIZE && state.mapMode != MapMode.LIGHT) {
+		if (tileSize >= FAST_MODE_TILE_SIZE && state.mapMode != MapMode.LIGHT) {
 
 			renderState.gameViewState = state;
 			renderState.world = game.world;
 			renderState.mapMode = state.mapMode;
 			renderState.g = (Graphics2D) g;
 			renderState.faction = state.faction;
-			renderState.tileSize = frozenTileSize;
-			renderState.draww = frozenTileSize;
-			renderState.drawh = frozenTileSize;
+			renderState.tileSize = tileSize;
+			renderState.draww = tileSize;
+			renderState.drawh = tileSize;
 
 			for (RenderingStep step : pipelines[state.mapMode.ordinal()].steps) {
 				step.render(renderState);
@@ -229,7 +221,7 @@ public class VanillaDrawer extends Drawer {
 						if (tile == null) {
 							continue;
 						}
-						Point drawAt = RenderingFunctions.getDrawingCoords(tile.getLocation(), state.tileSize);
+						Point drawAt = RenderingFunctions.getDrawingCoords(tile.getLocation(), tileSize);
 						step.render(renderState, tile, drawAt);
 					}
 				}
@@ -245,9 +237,9 @@ public class VanillaDrawer extends Drawer {
 		if (game.world == null) {
 			return null;
 		}
-		Position offsetTile = getWorldCoordOfPixel(new Point(0, 0), state.viewOffset, state.tileSize);
+		Position offsetTile = getWorldCoordOfPixel(new Point(0, 0), state.viewOffset, state.volatileTileSize);
 		Position offsetTilePlusCanvas = getWorldCoordOfPixel(
-				new Point(canvas.getWidth(), canvas.getHeight()), state.viewOffset, state.tileSize);
+				new Point(canvas.getWidth(), canvas.getHeight()), state.viewOffset, state.volatileTileSize);
 		return new Position[] {
 				offsetTile,
 				new Position(offsetTile.x, offsetTilePlusCanvas.y),
@@ -280,9 +272,9 @@ public class VanillaDrawer extends Drawer {
 	public void zoomView(int scroll, int mx, int my) {
 		int newTileSize;
 		if (scroll > 0) {
-			newTileSize = (int) ((state.tileSize - 1) * 0.95);
+			newTileSize = (int) ((state.volatileTileSize - 1) * 0.95);
 		} else {
-			newTileSize = (int) ((state.tileSize + 1) * 1.05);
+			newTileSize = (int) ((state.volatileTileSize + 1) * 1.05);
 		}
 		zoomViewTo(newTileSize, mx, my);
 	}
@@ -290,9 +282,9 @@ public class VanillaDrawer extends Drawer {
 	@Override
 	public void zoomViewTo(int newTileSize, int mx, int my) {
 		if (newTileSize > 0) {
-			Position tile = getWorldCoordOfPixelWithoutOffset(new Point(mx, my), state.viewOffset, state.tileSize);
-			state.tileSize = newTileSize;
-			Position focalPoint = tile.multiply(state.tileSize).subtract(state.viewOffset);
+			Position tile = getWorldCoordOfPixelWithoutOffset(new Point(mx, my), state.viewOffset, state.volatileTileSize);
+			state.volatileTileSize = newTileSize;
+			Position focalPoint = tile.multiply(state.volatileTileSize).subtract(state.viewOffset);
 			state.viewOffset.x -= mx - focalPoint.x;
 			state.viewOffset.y -= my - focalPoint.y;
 		}

src/ui/view/GameView.java
@@ -53,7 +53,7 @@ public class GameView {
 	public class GameViewState {
 		public long previousTickTime;
 		public volatile Position viewOffset = new Position(0, 0);
-		public volatile int tileSize = Settings.DEFAULT_TILE_SIZE;
+		public volatile int volatileTileSize = Settings.DEFAULT_TILE_SIZE;
 		public boolean fpMode;
 		
 		public Point mousePressLocation;
@@ -126,7 +126,7 @@ public class GameView {
 					fpsMouseRobot.mouseMove(drawingCanvas.getLocationOnScreen().x + currentMouse.x, 
 							drawingCanvas.getLocationOnScreen().y + currentMouse.y);
 				}
-				mouseOver(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.tileSize));
+				mouseOver(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.volatileTileSize));
 				state.previousMouse = currentMouse;
 			}
 
@@ -148,11 +148,11 @@ public class GameView {
 						currentActiveDrawer.rotateView(dx, dy);
 					}
 					if (state.leftMouseDown) {
-						state.boxSelect[1] = currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.tileSize);
+						state.boxSelect[1] = currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.volatileTileSize);
 					}
 					state.previousMouse = currentMouse;
 				}
-				mouseOver(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.tileSize));
+				mouseOver(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.volatileTileSize));
 			}
 		};
 		MouseListener mouseListener = new MouseListener() {
@@ -161,14 +161,14 @@ public class GameView {
 				Point currentMouse = e.getPoint();
 				if (!state.draggingMouse) {
 					if (SwingUtilities.isRightMouseButton(e)) {
-						rightClick(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.tileSize), shiftDown);
+						rightClick(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.volatileTileSize), shiftDown);
 					} else if (SwingUtilities.isLeftMouseButton(e)) {
-						leftClick(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.tileSize), shiftDown);
+						leftClick(currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.volatileTileSize), shiftDown);
 					}
 				} else {
 					if (SwingUtilities.isLeftMouseButton(e)) {
-						state.boxSelect[0] = currentActiveDrawer.getWorldCoordOfPixel(state.mousePressLocation, state.viewOffset, state.tileSize);
-						state.boxSelect[1] = currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.tileSize);
+						state.boxSelect[0] = currentActiveDrawer.getWorldCoordOfPixel(state.mousePressLocation, state.viewOffset, state.volatileTileSize);
+						state.boxSelect[1] = currentActiveDrawer.getWorldCoordOfPixel(currentMouse, state.viewOffset, state.volatileTileSize);
 						selectInBox(state.boxSelect[0], state.boxSelect[1], shiftDown);
 					}
 				}
@@ -190,7 +190,7 @@ public class GameView {
 				if (SwingUtilities.isLeftMouseButton(e)) {
 					state.leftMouseDown = true;
 					state.mousePressLocation = e.getPoint();
-					state.boxSelect[0] = currentActiveDrawer.getWorldCoordOfPixel(state.mousePressLocation, state.viewOffset, state.tileSize);
+					state.boxSelect[0] = currentActiveDrawer.getWorldCoordOfPixel(state.mousePressLocation, state.viewOffset, state.volatileTileSize);
 				} else if (SwingUtilities.isRightMouseButton(e)) {
 					rightMouseDown = true;
 				} else if (SwingUtilities.isMiddleMouseButton(e)) {
@@ -805,7 +805,7 @@ public class GameView {
 	 */
 	public void minimapMoveViewTo(double ratiox, double ratioy) {
 		Position tile = new Position(ratiox * game.world.getWidth(), ratioy * game.world.getHeight());
-		centerViewOnPixel(tile.multiply(state.tileSize));
+		centerViewOnPixel(tile.multiply(state.volatileTileSize));
 	}
 	
 	/** Adjusts the camera view so that the center of the specified tileloc is in the center
@@ -813,7 +813,7 @@ public class GameView {
 	 */
 	public void centerViewOnTile(TileLoc tileloc) {
 		Position worldPos = new Position(tileloc.x()+0.5, tileloc.y()+0.5);
-		Point pixel = currentActiveDrawer.getPixelOfWorldCoord(worldPos, state.tileSize);
+		Point pixel = currentActiveDrawer.getPixelOfWorldCoord(worldPos, state.volatileTileSize);
 		centerViewOnPixel(new Position(pixel.x, pixel.y));
 	}
 