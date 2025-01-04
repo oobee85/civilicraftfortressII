f7484e0e86b9fa240e9212102072400e50790ecb
nick <nkorshak@ucsc.edu>
2025-01-03 16:39:18 -0800

    increased cost of bricks in research, increased effect of distance on sound volume reduction

--- src/game/ItemType.java
+++ src/game/ItemType.java
@@ -6,17 +6,17 @@ import utils.*;
 
 public enum ItemType {
 
-	FOOD ( "Images/itemicons/wheat.png"),
-	WOOD ( "Images/itemicons/wood.png"),
+	FOOD  ( "Images/itemicons/wheat.png"),
+	WOOD  ( "Images/itemicons/wood.png"),
 	STONE ( "Images/itemicons/rock.png"),
 	MAGIC ( "Images/itemicons/magic.png"),
 	
-	HORSE ( "Images/units/horse.png"),
-	GRIFFIN ( "Images/units/griffin.png"),
-	ENT ( "Images/units/ent.png"),
-	VAMPIRE ( "Images/units/vampire.png"),
+	HORSE    ( "Images/units/horse.png"),
+	GRIFFIN  ( "Images/units/griffin.png"),
+	ENT 	 ( "Images/units/ent.png"),
+	VAMPIRE  ( "Images/units/vampire.png"),
 	WEREWOLF ( "Images/units/werewolf.png"),
-	WOLF ( "Images/units/wolf.png"),
+	WOLF 	 ( "Images/units/wolf.png"),
 	
 	CLAY       ( "Images/itemicons/clay.png"),
 	COAL       ( "Images/itemicons/coal.png"),
@@ -26,27 +26,26 @@ public enum ItemType {
 	IRON_ORE   ( "Images/itemicons/iron_ore.png"),
 	MITHRIL_ORE( "Images/itemicons/mithril_ore.png"),
 	
-	ADAMANTITE_ORE ( "Images/itemicons/adamantite_ore.png"),
-	RUNITE_ORE ( "Images/itemicons/runite_ore.png"),
-	TITANIUM_ORE ( "Images/itemicons/titanium_ore.png"),
+	ADAMANTITE_ORE  ( "Images/itemicons/adamantite_ore.png"),
+	RUNITE_ORE 		( "Images/itemicons/runite_ore.png"),
+	TITANIUM_ORE 	( "Images/itemicons/titanium_ore.png"),
 	
-	BRICK 	   	   ("SMITHY",     makeCosts(CLAY,50),	"Images/itemicons/brick.png"),
-	BRONZE_BAR 	   ("SMITHY",     makeCosts(COPPER_ORE,5, SILVER_ORE, 5),	"Images/itemicons/bronze_bar.png"),
-	GOLD_BAR 	   ("SMITHY",     makeCosts(GOLD_ORE, 10), 				"Images/itemicons/gold_bar.png"),
-	IRON_BAR 	   ("SMITHY",     makeCosts(COAL,10, 	IRON_ORE,10),		"Images/itemicons/iron_bar.png"),
-	MITHRIL_BAR    ("SMITHY",     makeCosts(COAL,20, 	MITHRIL_ORE,20),	"Images/itemicons/mithril_bar.png"),
-	
+	BRICK 	   	   ("SMITHY",  makeCosts(CLAY,50),						"Images/itemicons/brick.png"),
+	BRONZE_BAR 	   ("SMITHY",  makeCosts(COPPER_ORE,5, SILVER_ORE, 5),	"Images/itemicons/bronze_bar.png"),
+	GOLD_BAR 	   ("SMITHY",  makeCosts(GOLD_ORE, 10), 				"Images/itemicons/gold_bar.png"),
+	IRON_BAR 	   ("SMITHY",  makeCosts(COAL,10, 	IRON_ORE,10),		"Images/itemicons/iron_bar.png"),
+	MITHRIL_BAR    ("SMITHY",  makeCosts(COAL,20, 	MITHRIL_ORE,20),	"Images/itemicons/mithril_bar.png"),
 	ADAMANTITE_BAR ("SMITHY",  makeCosts(COAL,50, 	ADAMANTITE_ORE,50),	"Images/itemicons/adamantite_bar.png"),
-	RUNITE_BAR 	   ("SMITHY",  makeCosts(COAL,100, 	RUNITE_ORE,100),		"Images/itemicons/runite_bar.png"),
+	RUNITE_BAR 	   ("SMITHY",  makeCosts(COAL,100, 	RUNITE_ORE,100),	"Images/itemicons/runite_bar.png"),
 	TITANIUM_BAR   ("SMITHY",  makeCosts(COAL,100, 	TITANIUM_ORE,100),	"Images/itemicons/titanium_bar.png"),
 	
-	BETTER_WEAPONS   ("RESEARCH_LAB",  makeCosts(WOOD,2000),	"Images/interfaces/upgrades/better_weapons.png", "Increases attack damage of all units by : 25"),
+	BETTER_WEAPONS   	("RESEARCH_LAB",  makeCosts(WOOD,2000),	"Images/interfaces/upgrades/better_weapons.png", "Increases attack damage of all units by : 25"),
 	IMPROVED_SPARRING   ("RESEARCH_LAB",  makeCosts(FOOD,2000),	"Images/interfaces/upgrades/improved_sparring.png", "Decreases attack delay of all units by : 5"),
-	SHIELDS   ("RESEARCH_LAB",  makeCosts(WOOD,2000),	"Images/interfaces/upgrades/shields.png", "Increases health of all units by : 100"),
-	MEDICINE   ("RESEARCH_LAB",  makeCosts(FOOD,1000),	"Images/interfaces/upgrades/medicine.png", "Decreases ticks to heal of all units by : 25"),
+	SHIELDS   			("RESEARCH_LAB",  makeCosts(WOOD,2000),	"Images/interfaces/upgrades/shields.png", "Increases health of all units by : 100"),
+	MEDICINE   			("RESEARCH_LAB",  makeCosts(FOOD,1000),	"Images/interfaces/upgrades/medicine.png", "Decreases ticks to heal of all units by : 25"),
 	BETTER_FORMATIONS   ("RESEARCH_LAB",  makeCosts(FOOD,2000),	"Images/interfaces/upgrades/better_formations.png", "Decreases ticks to move of all units by : 5"),
-	FASTER_TRAINING   ("RESEARCH_LAB",  makeCosts(FOOD,2000),	"Images/interfaces/upgrades/faster_training.png", "Decreases the ticks to produce units by : 50"),
-	UNDYING_ARMY   ("RESEARCH_LAB",  makeCosts(FOOD,10000),	"Images/interfaces/upgrades/undying_army.png", "Gives all units lifesteal"),
+	FASTER_TRAINING   	("RESEARCH_LAB",  makeCosts(FOOD,2000),	"Images/interfaces/upgrades/faster_training.png", "Decreases the ticks to produce units by : 50"),
+	UNDYING_ARMY   		("RESEARCH_LAB",  makeCosts(FOOD,10000),"Images/interfaces/upgrades/undying_army.png", "Gives all units lifesteal"),
 	
 	;
 	
